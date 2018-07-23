34c7e087cf69a85f2da72d8e92b33782152f89bb
DESKTOP-4Q8QE20\JackChen <henrychen@aispeech.com>
2018-07-24 00:05:28 +0800

    jack录音机

---
+++ JackRecorder/src/main/java/com/jack/recorder/activity/ListActivity.java
@@ -0,0 +1,38 @@
+package com.jack.recorder.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.jack.recorder.R;
+import com.jack.recorder.adapter.FileListAdapter;
+import com.jack.recorder.record.FileUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by HXL on 16/8/11.
+ */
+public class ListActivity extends Activity {
+    ListView listView;
+    List<File> list = new ArrayList<>();
+    FileListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        listView = (ListView) findViewById(R.id.listView);
+        if("pcm".equals(getIntent().getStringExtra("type"))){
+            list= FileUtils.getPcmFiles();
+        }else{
+            list=FileUtils.getWavFiles();
+        }
+
+        adapter = new FileListAdapter(this, list);
+        listView.setAdapter(adapter);
+
+    }
+}
